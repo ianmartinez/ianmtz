89eb93f72e0ace4a3e55a14a677461273dc58d84
Ian Martinez <ianmartinez97@live.com>
2020-04-26 15:21:13 -0500

    Add language pad page

---
+++ ianmtz/ianmtz/Controllers/AppsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ianmtz.Controllers
+{
+    public class AppsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LanguagePad()
+        {
+            return View();
+        }
+    }
+}
