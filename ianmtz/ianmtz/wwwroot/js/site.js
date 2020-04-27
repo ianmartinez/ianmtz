c53ffea9d399daf50e8f0299b545e95234e0c6b0
Ian Martinez <ianmartinez97@live.com>
2020-04-26 22:24:01 -0500

    Implement lightboxes

--- ianmtz/ianmtz/wwwroot/js/site.js
+++ ianmtz/ianmtz/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿
+﻿/**
+ * Show a lightbox for each image with the attribute lightbox
+ * when the image is clicked
+ */
+function createLightboxes() {
+    let lightboxElements = document.querySelectorAll("[lightbox]");
+
+    for (let lightboxElement of lightboxElements) {
+        let lightboxImage = document.createElement("img");
+        lightboxImage.src = lightboxElement.src;
+
+        lightboxElement.onclick = function () {
+            let lightboxModal = document.getElementById("lightboxModal");
+            let lightboxModalBody = document.querySelector("#lightboxModal .modal-body");
+            lightboxModalBody.innerHTML = "";
+            lightboxModalBody.appendChild(lightboxImage);
+            $(lightboxModal).modal("show");
+        };
+    }
+}
+
+// Create lightboxes on page load
+document.addEventListener("DOMContentLoaded", createLightboxes);
