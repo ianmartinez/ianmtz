a1b47f401e4746af0355804c3ec5c5d766bd0117
Ian Martinez <ianmartinez97@live.com>
2020-04-27 12:54:49 -0500

    Fix issue with lightboxes clipping

---
+++ ianmtz/ianmtz/Controllers/LanguagePadController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ianmtz.Controllers
+{
+    public class LanguagePadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
