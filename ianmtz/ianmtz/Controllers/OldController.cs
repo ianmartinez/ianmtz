fd3e9da8c5f323debfa39db2cb1c90990dcd5f91
Ian Martinez <ianmartinez97@live.com>
2020-05-15 22:08:00 -0500

    Add old section

---
+++ ianmtz/ianmtz/Controllers/OldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ianmtz.Controllers
+{
+    public class OldController : Controller
+    {
+         public IActionResult ChameleonClassic()
+        {
+            return View();
+        }
+        
+        public IActionResult Screensavers()
+        {
+            return View();
+        }
+    }
+}
