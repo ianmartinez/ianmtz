e96ced16cd17e54e30089b254b4bd5b4ade9a049
Ian Martinez <ianmartinez97@live.com>
2020-05-17 17:00:47 -0500

    Add stacks demo

---
+++ ianmtz/ianmtz/Controllers/TamarackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ianmtz.Controllers
+{
+    public class TamarackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
