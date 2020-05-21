182cb23f50763a01c15c6f22d8c43dd6d3059a91
Ian Martinez <ianmartinez97@live.com>
2020-05-21 12:36:06 -0500

    Add cardinal qemu page

---
+++ ianmtz/ianmtz/Controllers/CardinalQemuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ianmtz.Controllers
+{
+    public class CardinalQemuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
