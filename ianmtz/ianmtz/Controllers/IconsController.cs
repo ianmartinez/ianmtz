88434e47ce55e979364f1d38047370c02f5a4941
Ian Martinez <ianmartinez97@live.com>
2020-05-20 22:22:11 -0500

    Add tlell icons

--- ianmtz/ianmtz/Controllers/IconsController.cs
+++ ianmtz/ianmtz/Controllers/IconsController.cs
@@ -4,12 +4,17 @@ namespace ianmtz.Controllers
 {
     public class IconsController : Controller
     {
-        public IActionResult Tlell()
+        public IActionResult Horizon()
         {
             return View();
         }
 
         public IActionResult Taiga()
+        {
+            return View();
+        }
+
+        public IActionResult Tlell()
         {
             return View();
         }
