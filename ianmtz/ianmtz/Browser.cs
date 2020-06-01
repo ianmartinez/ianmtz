f23a372e0271f11b075cd54982bdab55c175c185
Ian Martinez <ianmartinez97@live.com>
2020-06-01 15:07:43 -0500

    Make somewhat presentable in IE

---
+++ ianmtz/ianmtz/Browser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace ianmtz
+{
+    /// <summary>
+    /// Analyze the user agent for compatibility.
+    /// </summary>
+    public class Browser
+    {
+        private readonly HttpRequest Request;
+
+        /// <summary>
+        /// Create a browser object from a HttpRequest.
+        /// </summary>
+        /// 
+        /// <param name="Request">The request.</param>
+        public Browser(HttpRequest Request)
+        {
+            this.Request = Request;
+        }
+
+        /// <summary>
+        /// Get the user agent string.
+        /// </summary>
+        public string UserAgent
+        {
+            get
+            {
+                return Request.Headers[HeaderNames.UserAgent];
+            }
+        }
+
+        /// <summary>
+        /// If the browser in the user agent string identifies as Internet Explorer.
+        /// </summary>
+        public bool IsIE
+        {
+            get
+            {
+                if (UserAgent == null)
+                    return false;
+
+                return UserAgent.ToLower().Contains("msie") || UserAgent.ToLower().Contains("trident");
+            }
+        }
+    }
+}
